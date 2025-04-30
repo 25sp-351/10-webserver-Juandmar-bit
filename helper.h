c2e9c97e44bb6c9b6ea13814bb22c583852e261c
Juandmar-bit <diegomartinezvasquez5@gmail.com>
2025-04-30 10:09:39 -0700

    Update helper.h

--- helper.h
+++ helper.h
@@ -4,8 +4,5 @@
 const char *get_mime_type(const char *extension);
 int cal(const char *operand, const int first, const int second);
 void not_found (const int accepted_socket);
-void cal_path(const int accepted_socket, const char *path_file);
-void static_path(const int accepted_socket, const char *path_file, const char *real_path);
-void sec_path (const int accepted_socket, const char *path_file);
 
 #endif
